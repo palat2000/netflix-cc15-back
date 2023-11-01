05fe8276c1f6d43d126e26ee84543a1e5e24ef07
pandaree <pandaree.c51@gmail.com>
2023-11-01 17:31:32 +0700

    send allUserProfile when login

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
+// app.use("/admin", adminRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
