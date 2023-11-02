424d4f99864ae8259a7dc67e023f08486f4567bc
palat2000 <palat.tungsantitham@gmail.com>
2023-11-02 09:45:05 +0700

    dasda

--- src/utils/create-error.js
+++ src/utils/create-error.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-const createError = (message, status) => {
-  const error = new Error(message);
-  error.statusCode = status;
-=======
 const createError = (message, statusCode) => {
   const error = new Error(message);
   error.statusCode = statusCode;
->>>>>>> develop
   return error;
 };
 
