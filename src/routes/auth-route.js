6004113e88fd189eed7542986dc7613e9400e702
pandaree <pandaree.c51@gmail.com>
2023-11-08 10:09:12 +0700

    edit chooseprofile to be post

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -6,6 +6,6 @@ const authenticateMiddleware = require("../middlewares/authenticate");
 router.post("/login", authController.login);
 router.post("/register", authController.register);
 router.get("/me", authenticateMiddleware, authController.getMe);
-router.get("/profile", authenticateMiddleware, authController.chooseProfile);
+router.post("/profile", authenticateMiddleware, authController.chooseProfile);
 router.post("/checkemail", authController.checkEmail);
 module.exports = router;
